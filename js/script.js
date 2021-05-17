9aa9e2b2b836e35d4bc8c0dc4973e73b520ae982
Nivan Jr <78574377+nivslive@users.noreply.github.com>
2021-05-17 19:44:53 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+alert('salve caraio');
+
+console.log('oi')
+
+
+let open = document.getElementById('openModal');
+let close = document.getElementById('close');
+let fade = document.getElementById('fade');
+let cntModal = document.getElementById('ctnModal');
+
+open.onclick = function() {fade.style.display = "flex }
+
+close.onclick = function() {fade.style.display = "none"}
+
+fade.onclick = function() {fade.style.display = "none"}
+
+cntModal.onclick = function(event) {event.stopPropagation()}
